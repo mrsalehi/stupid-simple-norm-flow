f07b2e70642ad862678792f968fe90a7acf8848a
mrsalehi <mrezasal1@gmail.com>
2021-03-29 09:00:20 -0400

    Model creation unit test passed

--- models.py
+++ models.py
@@ -7,18 +7,16 @@ from flows import *
 
 
 class FCNEncoder(nn.Module):
-    def __init__(self, hidden_sizes: List[int], dim_input: int, activation=nn.ReLU):
+    def __init__(self, hidden_sizes: List[int], dim_input: int, activation=nn.ReLU()):
         super().__init__()
         
-        layers = []
         
         hidden_sizes = [dim_input] + hidden_sizes
-        
-        for i in range(len(hidden_sizes)):
-            layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i+1]))
-            layers.append(activation)
+        self.net = nn.ModuleList()
 
-        self.net = nn.ModuleList(*layers)
+        for i in range(len(hidden_sizes) - 1):
+            self.net.append(nn.Linear(hidden_sizes[i], hidden_sizes[i+1]))
+            self.net.append(activation)
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
@@ -28,16 +26,14 @@ class FlowModel(nn.Module):
     def __init__(self, flows: List[str], D: int, activation=F.elu):
         super().__init__()
         
-        self.layers = []
+        self.prior = MultivariateNormal(torch.zeros(D), torch.eye(D))
+        self.net = nn.ModuleList()
 
         for i in range(len(flows)):
             layer_class = eval(flows[i])
-            self.layers.append(layer_class(D, activation))
+            self.net.append(layer_class(D, activation))
 
-        self.layers = nn.ModuleList(self.layers)
         self.D = D
-        self.prior = MultivariateNormal(torch.zeros(D), torch.diagonal(1))
-        self.flow_net = None  # TODO: Specify the architecture of flownet later
 
 
     def forward(self, mu: torch.Tensor, sigma: torch.Tensor):
@@ -45,7 +41,7 @@ class FlowModel(nn.Module):
         mu: tensor with shape (batch_size, D)
         sigma: tensor with shape (batch_size, D)
         """
-        batch_size = x.shape[0]
+        batch_size = mu.shape[0]
         samples = self.prior(mu, sigma * torch.eye(D)).sample(batch_size)
         z = samples * sigma + mu 
         
@@ -58,18 +54,15 @@ class FlowModel(nn.Module):
 
 
 class FCNDecoder(nn.Module):
-    def __init__(self, hidden_sizes: List[int], dim_input: int, activation=nn.ReLU):
+    def __init__(self, hidden_sizes: List[int], dim_input: int, activation=nn.ReLU()):
         super().__init__()
         
-        layers = []
-        
         hidden_sizes = [dim_input] + hidden_sizes
-        
-        for i in range(len(hidden_sizes)):
-            layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i+1]))
-            layers.append(activation)
+        self.net = nn.ModuleList()
 
-        self.net = nn.ModuleList(*layers)
+        for i in range(len(hidden_sizes) - 1):
+            self.net.append(nn.Linear(hidden_sizes[i], hidden_sizes[i+1]))
+            self.net.append(activation)
 
     def forward(self, z: torch.Tensor):
         return self.net(z)
